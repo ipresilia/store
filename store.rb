02fb35b5de164b335a57efdede54d6a243d17b12
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-17 16:45:15 +0200

    Added a couple more Methods and called them

--- store.rb
+++ store.rb
@@ -5,11 +5,11 @@ puts "\n"
 end
 
 # Shopping Cart
-shopping_cart = []
+@shopping_cart = []
 
 
 # List of Products
-products = [
+@products = [
   { reference_number: 123, name: "MacBook Air", price: 1200 },
   { reference_number: 124, name: "MacBook Pro", price: 2000 },
   { reference_number: 125, name: "Apple Watch", price: 700 },
@@ -19,12 +19,61 @@ products = [
 ]
 
 
-# Welcome user
+# Welcome user & Show products
+def welcome_n_product_show
 print_divider
-puts "Welcome to Apple Fan Boy!"
+  puts "What will you buying from us today?\n
+  This is what we have available:\n
+  Each product has its own reference number\n
+  which you can use to select the products with."
 print_divider
-puts "What will you buying from us today?\n
-This is what we have available: "
-products.each do |product|
-  puts "#{product[:name]} €#{product[:price]}"
+  @products.each do |product|
+    puts "#{product[:reference_number]} - #{product[:name]} €#{product[:price]}"
+  end
+end
+
+
+#User Selection
+def select_product
+  puts "Select the item(s) you want to buy by using their number of reference: "
+  gets.chomp.to_i
+end
+
+
+# Add Selection to cart
+def add_to_cart(reference_number)
+  product = look_for_product(reference_number)
+  if product != nil
+    @shopping_cart << product
+    puts "Congrats. The following has been added to the cart successfully: '#{product[:name]}' costing €#{product[:price]}"
+  else
+    puts "Invalid selection. Please try again with an existing reference_number."
+    print_divider
+  end
+end
+
+
+# Check and see whether input matches reference_number
+def look_for_product(reference_number)
+  @products.each do |product|
+    if product[:reference_number] == reference_number.to_i
+      return product
+    end
+  end
+end
+
+
+
+
+
+
+
+
+# Visit store (Used to print methods)
+loop do
+  print_divider
+  puts "Welcome to Apple Fan Boy!\n\n"
+  welcome_n_product_show
+  product_reference_number = select_product
+  add_to_cart(product_reference_number)
 end
